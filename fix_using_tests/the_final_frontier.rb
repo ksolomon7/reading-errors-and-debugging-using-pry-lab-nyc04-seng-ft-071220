01000b3de0ab8004ef061ff3bf7c0ce43add428d
Kalkidan Taye <kalkidan.solomon2@gmail.com>
2020-07-15 13:19:17 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -5,13 +5,16 @@ def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
 
+date=generate_star_date
+
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
-binding.pry
 
 def engage
   puts state_log(date)
   binding.pry
   date = generate_star_date
 end
+
+binding.pry
