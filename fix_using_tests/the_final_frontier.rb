e3f7659fb6dfc671fc1074fddbf3bf2aec2e5a05
Kalkidan Taye <kalkidan.solomon2@gmail.com>
2020-07-15 13:06:07 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,8 +1,8 @@
 # don't forget to add: require 'pry'
 require 'pry'
+
 def generate_star_date
   (rand(100000) + 400000) / 10.0
-  binding.pry
 end
 
 
@@ -12,5 +12,6 @@ end
 
 def engage
   puts state_log(date)
+  binding.pry
   date = generate_star_date
 end
